55102fd92cdcd1eb8469944ddd3819b7bc7ea6a3
Ryan Hellgren <ryanhellgren@gmail.com>
2025-09-15 12:29:14 +0200

    Lade till ett mycket grundläggande skeleton som programmet kan bygga vidare på

---
+++ banken.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class banken {
+    static void meny (){
+
+    }
+
+    static void pengar (){
+
+    }
+
+    static void user (){
+
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menu");
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1:
+                    
+                    break;
+                default:
+                    
+                break;
+            }
+            scan.close();
+        }
+    }
+}
